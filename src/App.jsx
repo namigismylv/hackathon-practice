af8f201af82cb39a152fce6befe7e86e5b4f01a9
namigismylv <tu7ew05s4@code.edu.az>
2025-12-10 15:41:15 +0400

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 import MainContext from "./Context/Context";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../public/BASE_URL";
 // import { useMemo, useState } from "react";
 
 function App() {
   // const [user, setUser] = useState(null);
-
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const axiosData = async () => {
+      try{
+        const responses = await axios.all([
+          axios.get(`${BASE_URL}/products`)
+        ]);
+        const [resProducts] = responses
+        setProducts(resProducts.data.data)
+        console.log(resProducts.data.data)
+      }
+      catch (error){
+        console.log("Fetching Problem",error)
+      }
+    };
+    axiosData()
+  }, []);
   const contextData = {
-
+    products,
+    setProducts,
+    error,
+    setError
   };
   return (
     <MainContext.Provider value={contextData}>
