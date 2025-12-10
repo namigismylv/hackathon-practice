6b7edde86896cc18dec348b98ef29868b9de00ce
Ramin105 <rmnselimli@gmail.com>
2025-12-10 16:21:02 +0400

    categories api update

--- src/Admin/CategoryList.jsx
+++ src/Admin/CategoryList.jsx
@@ -1,16 +1,14 @@
 import { useContext } from "react";
-import  MainContext  from "../Context/Context";
+import MainContext from "../Context/Context";
 
 const CategoryList = () => {
   const { categories } = useContext(MainContext);
 
- 
-
   return (
-  <ul>
-      {categories.map((c) => (
-        <li key={c.id}>{c.name}</li>
-      ))}
+    <ul>
+      {categories.map((c,index) => {
+        return <li key={index}>{c.name}</li>;
+      })}
     </ul>
   );
 };
